185a4e4ddf97780d6460b1fbc5377ef9150c16df
hectorBWA <hectorzavala12345@gmail.com>
2018-06-08 10:59:08 -0700

    Types Recap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  any: any;
+  str: string = 'sharks are cool';
+  num: number = 5;
+  space: void;
+
+  chad: null;
+
+  arr: any[] = [4, 'hello', true, false];
+  arrTwo: number[] = [5, 7];
+  arrThree: Array<number> = [4, 6];
+  arrFour: string[] = ['Hola'];
+  arrFive: Array<string> = ['hello'];
+
 }
+
